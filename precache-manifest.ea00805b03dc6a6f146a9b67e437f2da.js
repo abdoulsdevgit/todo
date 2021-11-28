dc38c31fd9cad5d6f9821be162b0b084ec82ee2d
abdoulsdevgit <abdoulsdevgit@gmail.com>
2021-11-27 20:57:43 -0500

    Updates

---
+++ precache-manifest.ea00805b03dc6a6f146a9b67e437f2da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa038f5fae3776af9bc4660ea4993f1",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "1cd313aec30da90fc986",
+    "url": "/todo/static/css/main.1f0ed328.chunk.css"
+  },
+  {
+    "revision": "a93f7caf7505cb99e5e5",
+    "url": "/todo/static/js/2.f0a2246d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.f0a2246d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd313aec30da90fc986",
+    "url": "/todo/static/js/main.b143f27e.chunk.js"
+  },
+  {
+    "revision": "67114fc4ad6958319bc3",
+    "url": "/todo/static/js/runtime-main.e29500e7.js"
+  }
+]);
